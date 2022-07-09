4376755dbe4a4d2ffec87797cea7dcf32561f0cd
schktjm <si2.o.mjt@gmail.com>
2022-07-09 22:41:56 +0900

    :recycle: URLの変更監視から title のobserve へ変更

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ const getDescriptionElement = () => {
   }
 };
 
+// TODO: 実行時に要素がなかったりするからこうしてるけど body を監視した方がいいかも
 const cronGetElement = (): Promise<Element | undefined> =>
   new Promise((resolve) => {
     const intervalId = setInterval(() => {
@@ -20,30 +21,73 @@ const cronGetElement = (): Promise<Element | undefined> =>
         resolve(descriptionElm);
         clearInterval(intervalId);
       }
-      console.log("not found");
     }, 1000);
   });
 
-const main = async () => {
-  const title = document.head.getElementsByTagName("title")[0]?.textContent;
+const isUserProfileTitle = (title: string) => {
   const regexp = /.*[\(\uFF08]+@\w*[\)\uFF09].*/; // ユーザープロフィールページかどうか
-  if (!(title && title.match(regexp))) {
-    return;
-  }
-  console.log("突破", title);
-
-  const descriptionElm = await cronGetElement();
-  console.log("await", descriptionElm);
-  if (!descriptionElm) {
-    console.error("not found user profile area.");
-    return;
-  }
-  console.log("description", descriptionElm);
-
-  const reactElm = document.createElement("div");
-  reactElm.setAttribute("id", "user-memo");
-  const accept = descriptionElm.appendChild(reactElm);
-  console.log(accept);
+  return regexp.test(title);
 };
 
-main();
+const useMutationObserver = (
+  targetElm: Element,
+  callback: (records: MutationRecord[], observer: MutationObserver) => void,
+  config?: MutationObserverInit
+) => {
+  const observer = new MutationObserver(callback);
+
+  const defaultConfig = {
+    childList: true,
+    attributes: true,
+  };
+
+  observer.observe(targetElm, { ...defaultConfig, ...config });
+};
+
+const observeTitleTag = (callback: Function) => {
+  const titleTag = document.head.getElementsByTagName("title")[0];
+
+  const observeCallback = (records: MutationRecord[]) => {
+    records.forEach((r) => {
+      console.log(r.type);
+      if (r.type === "childList") {
+        callback();
+      }
+    });
+  };
+
+  useMutationObserver(titleTag, observeCallback);
+};
+
+const main = async () => {
+  const cb = async () => {
+    const title = document.head.getElementsByTagName("title")[0]?.textContent;
+    if (!title || !isUserProfileTitle(title)) {
+      return;
+    }
+    console.log("突破", title);
+
+    const descriptionElm = await cronGetElement();
+    console.log("await", descriptionElm);
+    if (!descriptionElm) {
+      console.error("not found user profile area.");
+      return;
+    }
+
+    const target = document.getElementById("user-memo");
+    if (target) {
+      return;
+    }
+
+    const reactElm = document.createElement("div");
+    reactElm.setAttribute("id", "user-memo");
+    descriptionElm.appendChild(reactElm);
+  };
+
+  observeTitleTag(cb);
+};
+
+// 鍵垢だと title さえ undefined になるためちょっと待つ
+setTimeout(() => {
+  main();
+}, 100);
