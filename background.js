2cc7df8d815da9daf28157ac06fb6ce2b2997eea
schktjm <si2.o.mjt@gmail.com>
2022-07-09 15:09:40 +0900

    :+1: first commit

---
+++ background.js
@@ -0,0 +1,22 @@
+// background.js
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log({
+    tabId: tabId,
+    changeInfo: changeInfo,
+    tab: tab,
+    tabUrl: tab.url,
+    locationHref: location.href,
+    this: this,
+  });
+  if (
+    changeInfo.status === "complete" &&
+    tab.url.indexOf("https://twitter.com/") > -1
+  ) {
+    console.log(`updated: ${tab.url}`);
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["./src/main.js"],
+    });
+  }
+});
---
+++ src/main.js
@@ -0,0 +1,5 @@
+console.log("on twitter");
+
+const head = document.head;
+const title = head.getElementsByTagName("title");
+console.log(title[0]);
