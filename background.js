2aed56e7fa105ca6c1c45cde957c0aa08d9834d0
schktjm <schktjm@gmail.com>
2023-05-27 23:35:43 +0900

    :construction: Mac 初期化するからとりあえずあげとく

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
 // background.js
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log({
+    tabId: tabId,
+    changeInfo: changeInfo,
+    tab: tab,
+    tabUrl: tab.url,
+    locationHref: location.href,
+  });
+  if (
+    changeInfo.status === "complete" &&
+    tab.url.indexOf("https://www.google.com/") > -1
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["./dist/main.js"],
+    });
+  }
+});
